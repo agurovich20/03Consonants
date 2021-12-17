c9b23916895255d56013081ad38dbe6224e56787
Oscar Breen <90660886+Ozb888@users.noreply.github.com>
2021-12-17 14:05:38 -0500

    Update BinSearch.java

--- BinSearch.java
+++ BinSearch.java
@@ -1,2 +1,12 @@
 public class BinSearch{
   
+  private int _data
+  private int _size();
+  private int _target;
+  
+  public static void Binsearch(int[] a){
+    _hi = a.size();
+    _lo = 0;
+    
+    while( true ){
+      if(
