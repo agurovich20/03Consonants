17550721f81ac08ee1188f0c3e06bf305dd1d06e
Oscar Breen <90660886+Ozb888@users.noreply.github.com>
2021-12-19 18:10:06 -0500

    lab

---
+++ SearchDriver.java
@@ -0,0 +1,16 @@
+public class SearchDriver{
+  public static void main(String[] args){
+    Comparable[] one = new Integer[100000];
+    Comparable[] two = new Integer[200000];
+    Comparable[] three = new Integer[500000];
+    Comparable[] four = new Integer[1000000];
+    ArrayList<>
+
+    for (int i = 0; i < one.length; i++) {
+      one[i] = i;
+    }
+    long startTime = System.currentTimeMillis();
+
+    long endTime = System.currentTimeMillis();
+  }
+}
