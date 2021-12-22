f3b42424d358d64c058e89cdf68894ddce7af50f
Oscar Breen <90660886+Ozb888@users.noreply.github.com>
2021-12-21 22:43:26 -0500

    Updates

--- SearchDriver.java
+++ SearchDriver.java
@@ -1,16 +1,90 @@
 public class SearchDriver{
-  public static void main(String[] args){
-    Comparable[] one = new Integer[100000];
-    Comparable[] two = new Integer[200000];
-    Comparable[] three = new Integer[500000];
-    Comparable[] four = new Integer[1000000];
-    ArrayList<>
+  static int _size = 100_000; //billion
+  static long timesf;
+  static Comparable[] array = makeArray(_size);
+  static int _trials = 100_000;
 
-    for (int i = 0; i < one.length; i++) {
-      one[i] = i;
-    }
-    long startTime = System.currentTimeMillis();
+ public static Comparable[] makeArray(int size){
 
-    long endTime = System.currentTimeMillis();
-  }
+	Comparable[] array = new Comparable[size];
+	for(int i=0;i<size;i++){
+		array[i] = i;
+	}
+    return array;
+ }
+
+ public static long BinTime() {
+	long past = System.currentTimeMillis();
+
+        for(Comparable target : array) {
+		BinSearch.binSearch(array, target);
+	}
+
+	long now = System.currentTimeMillis();
+	timesf = now - past;
+    return timesf;
+ }
+
+ public static double BinaryTimeAvg() {
+    return (double)(timesf) / _size;
+ }
+
+ public static long LinTime() {
+	long past = System.currentTimeMillis();
+
+	for(Comparable target : array) {
+		LinSearch.linSearch(array, target);
+	}
+
+	long now = System.currentTimeMillis();
+	timesf = now - past;
+   return timesf;
+ }
+
+public static long linWorst() {
+	long past = System.currentTimeMillis();
+
+	for(int i = 0; i < _trials; i++){
+		LinSearch.linSearch(array, _size -1);
+	}
+
+	long now = System.currentTimeMillis();
+	timesf = now - past;
+  return timesf;
+}
+
+
+public static long binWorst() {
+	long past = System.currentTimeMillis();
+
+	for(int i = 0; i < _trials; i++){
+		BinSearch.binSearch(array, _size -1);
+	}
+
+	long now = System.currentTimeMillis();
+	timesf = now - past;
+  return timesf;
+}
+
+ public static double LinearTimeAvg() {
+   return (double)(timesf) / _size;
+ }
+
+ public static void main(String[] args){
+	System.out.println("Binary time spent on a " + _size + " Array: " + BinTime() + " milliseconds");
+
+	System.out.println("Average time in milliseconds per search: " + BinaryTimeAvg() + "\n");
+
+	System.out.println("Linear time spent on a " + _size + " Array: " + LinTime() + " milliseconds");
+
+	System.out.println("Average time in milliseconds per search: " + LinearTimeAvg() + "\n");
+
+	System.out.println("Binary time spent on a worst case " + _size + " Array " + binWorst() + " milliseconds, for " + _trials + " trials");
+
+        System.out.println("Average time in milliseconds per search: " + BinaryTimeAvg() + "\n");
+
+        System.out.println("Linear time spent on a worst case " + _size + " Array " + linWorst() + " milliseconds, for " + _trials + " trials"); 
+
+        System.out.println("Average time in milliseconds per search: " + LinearTimeAvg() + "\n");
+ }
 }
